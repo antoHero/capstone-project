6c5d1cde5e1524833c7e6ede60610226d1dcd8e5
antoHero <veeqanto@gmail.com>
2019-11-30 23:23:52 +0100

    Implement the routes endpoint

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const userRoutes = require('./api/v1/routes/user');
 const articleRoutes = require('./api/v1/routes/article');
+const gifRoutes = require('./api/v1/routes/gif');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,6 +19,7 @@ app.get('/', (request, response) => {
 });
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/articles', articleRoutes);
+app.use('/api/v1/gifs', gifRoutes);
 /* router.get('api/v1/user', userCtrl.getUsers);
 router.get('api/v1/users/:id', userCtrl.id);
 router.post('api/v1/users', userCtrl.createUser);
